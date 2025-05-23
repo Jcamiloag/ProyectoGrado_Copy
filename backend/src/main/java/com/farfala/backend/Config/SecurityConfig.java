0a7fa0296f554138e0b4e3507385fb9e1c18f659
Juana Valentina Plaza <juana.plaza01@uceva.edu.co>
2025-05-22 20:44:16 -0500

    fix: modifica la clase SecurityConfig en la configuración del Cors

--- backend/src/main/java/com/farfala/backend/Config/SecurityConfig.java
+++ backend/src/main/java/com/farfala/backend/Config/SecurityConfig.java
@@ -33,7 +33,8 @@ public class SecurityConfig {
             .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .authorizeHttpRequests(authRequest ->
               authRequest
-                .requestMatchers("/auth/login", "/auth/register").permitAll()
+                .requestMatchers("/").permitAll() // Añade esta línea
+                .requestMatchers("/auth/**").permitAll()
                 .requestMatchers("/v1/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                 .anyRequest().authenticated()
             )
@@ -49,7 +50,8 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setExposedHeaders(Arrays.asList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
